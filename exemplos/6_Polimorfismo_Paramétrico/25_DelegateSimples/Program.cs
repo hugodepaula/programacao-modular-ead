eede369d8d5bb8ab629605fb66bae38106820237
Hugo de Paula <hugo@pucminas.br>
2021-08-06 16:41:36 -0300

    Acrescentando o exemplo de delegate simples

---
+++ exemplos/6_Polimorfismo_Paramétrico/25_DelegateSimples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _25_DelegateSimples
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Mensageiro.Exibir exibir = new Mensageiro.Exibir(MensagemConsole);
+      
+      exibir("Estou exibindo esta mensagem no console!");
+    }
+
+    public static void MensagemConsole(string mensagem)
+    {
+      Console.WriteLine(mensagem);
+    }
+  }
+
+  public class Mensageiro
+  {
+    public delegate void Exibir(string mensagem);
+  }
+}
