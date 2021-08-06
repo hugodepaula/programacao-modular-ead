98f918f84e35082bc4b3f6348f313dd262de00fd
Hugo de Paula <hugo@pucminas.br>
2021-08-06 18:55:10 -0300

    Acrescentando o exemplo de eventos com o alarme

---
+++ exemplos/6_Polimorfismo_Paramétrico/28_EventoAlarme/Alarme.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace _28_EventoAlarme
+{
+    class Alarme
+    {
+        // Declara um delegate Acordar que será usado como o callback do evento.
+        public delegate void Acordar(string mensagem); 
+        // Declara o evento TempoEsgotado que irá chamar os métodos Acordar de cada assinante
+        public event Acordar TempoEsgotado;
+
+        public void DisparaEvento()
+        {
+          AoEsgotarTempo();
+        }
+
+        public void AoEsgotarTempo() {
+          // Verifica se existe alguém que asinou o evento
+          if (TempoEsgotado != null)
+          {
+            TempoEsgotado("Tempo esgotado!");
+          }
+        }
+    }
+}
---
+++ exemplos/6_Polimorfismo_Paramétrico/28_EventoAlarme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _28_EventoAlarme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarme alarme = new Alarme();
+
+            // Registra o método de callback no alarme
+            alarme.TempoEsgotado += MensagemConsole;
+
+            // Ao disparar o evento, ele irá notificar os assinantes do evento tempo esgotado.
+            alarme.DisparaEvento();
+        }
+
+        public static void MensagemConsole(string mensagem) 
+        {
+            Console.WriteLine(mensagem);
+        }
+    }
+}
