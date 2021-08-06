7f60d946aff448533b6b18b72f92be262a5ee3d1
Hugo de Paula <hugo@pucminas.br>
2021-08-06 16:55:10 -0300

    Acrescentando o exemplo de funções lambda com Func<T, TResult>

---
+++ exemplos/6_Polimorfismo_Paramétrico/27_FuncoesLambdaComFunc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _26_FuncoesLambda
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Func<int, int, int> soma1 = (int x, int y) =>
+      {
+        int result = x + y;
+        return result;
+      };
+
+      Func<int, int, int> soma2 = (x, y) => (x + y);
+
+      Console.WriteLine("Soma1(2, 3) = {0}", soma1(2, 3));
+      Console.WriteLine("Soma2(5, 4) = {0}", soma2(5, 4));
+    }
+  }
+}
