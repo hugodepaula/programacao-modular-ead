dd419b64ddc7ab9e339c58aa40a6f81a6c37d40c
Hugo de Paula <hugo@pucminas.br>
2021-08-06 16:52:03 -0300

    Acrescentando o exemplo de funções lambda com delegate

---
+++ exemplos/6_Polimorfismo_Paramétrico/26_FuncoesLambda/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _26_FuncoesLambda
+{
+  class Program
+  {
+    public delegate int Soma(int x, int y);
+
+    static void Main(string[] args)
+    {
+      Soma soma1 = (int x, int y) =>
+      {
+        int result = x + y;
+        return result;
+      };
+
+      Soma soma2 = (x, y) => (x + y);
+
+      Console.WriteLine("Soma1(2, 3) = {0}", soma1(2, 3));
+      Console.WriteLine("Soma2(5, 4) = {0}", soma2(5, 4));
+    }
+  }
+}
